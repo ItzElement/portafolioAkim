5425c26d5e45629ddcd09d158d1c98a5f7cf8cbc
ItzElement <carlitosvargastorres@hotmail.com>
2025-12-05 03:46:40 -0600

    Implementación del 90% en colaboraciones [0.3.2]

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,16 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      animation: {
+        'scroll': 'scroll 40s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+
       fontFamily: {
         sans: ['Lexend', ...defaultTheme.fontFamily.sans],
         serif: ['Lora', ...defaultTheme.fontFamily.serif],
